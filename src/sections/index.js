93f954222ccfdc8620dd8a8a8ba07ad08d1effaa
divyom <divyomsing7@gmail.com>
2023-11-03 01:43:37 +0530

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from './Hero';
+import PopularProducts from './PopularProducts';
+import SuperQuality from './SuperQuality';
+import CustomerReviews from './CustomerReviews';
+import Subscribe from './Subscribe';
+import Footer from './Footer';
+import Services from './Services';
+import SpeicalOffer from './SpeicalOffer';
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    CustomerReviews,
+    Subscribe,
+    Footer,
+    Services,
+    SpeicalOffer
+}
